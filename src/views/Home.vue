80f5f5325d7fefcf9f88782567e0609edd7e4284
Ngoc Ngo <ngocnv371@gmail.com>
2020-09-13 09:30:19 +0700

    simplify markup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,66 @@
 <template>
-  <div class="home d-flex" style="height: 100%">
-    <div class="d-flex flex text-center">
-      <v-spacer></v-spacer>
-      <div class="d-flex flex-column">
-        <v-spacer></v-spacer>
-        <div class="d-flex align-center flex-column">
-          <div class="d-flex flex scene-title">
-            <h1
-              class="animate__animated animate__flipInY"
-              style="font-size: 7em"
-              :style="{ fontSize: config.title.fontSize }"
-            >
-              <VectorIcon
-                class="d-inline-block"
-                icon="baby"
-                :size="config.title.icon.size"
-              />
-              Baby Flip
-            </h1>
-          </div>
-          <div class="flex primary-controls">
-            <v-btn
-              icon
-              :width="config.play.size"
-              :height="config.play.size"
-              color="primary"
-              class="animate__animated animate__tada animate__infinite"
-              @click="$emit('play')"
-            >
-              <v-icon :size="config.play.size">mdi-play</v-icon>
-            </v-btn>
-          </div>
-          <div class="d-flex flex secondary-controls">
-            <v-btn
-              icon
-              color="primary"
-              x-large
-              class="ma-3 animate__animated animate__fadeIn animate__slower"
-              :width="config.button.size"
-              :height="config.button.size"
-            >
-              <v-icon>mdi-music</v-icon>
-            </v-btn>
-
-            <v-btn
-              icon
-              color="primary"
-              x-large
-              class="ma-3 animate__animated animate__fadeIn animate__slower"
-              :width="config.button.size"
-              :height="config.button.size"
-            >
-              <v-icon>mdi-information-outline</v-icon>
-            </v-btn>
-
-            <v-btn
-              icon
-              color="primary"
-              x-large
-              class="ma-3 animate__animated animate__fadeIn animate__slower"
-              :width="config.button.size"
-              :height="config.button.size"
-            >
-              <v-icon>mdi-facebook</v-icon>
-            </v-btn>
-          </div>
-        </div>
-        <v-spacer></v-spacer>
+  <div class="view-topics d-flex align-center" style="height: 100%">
+    <div class="d-flex flex align-center flex-column">
+      <div class="d-flex flex scene-title">
+        <h1
+          class="animate__animated animate__flipInY"
+          style="font-size: 7em"
+          :style="{ fontSize: config.title.fontSize }"
+        >
+          <VectorIcon
+            class="d-inline-block"
+            icon="baby"
+            :size="config.title.icon.size"
+          />
+          Baby Flip
+        </h1>
       </div>
-      <v-spacer></v-spacer>
+      <div class="flex primary-controls">
+        <v-btn
+          icon
+          :width="config.play.size"
+          :height="config.play.size"
+          color="primary"
+          class="animate__animated animate__tada animate__infinite"
+          @click="$emit('play')"
+        >
+          <v-icon :size="config.play.size">mdi-play</v-icon>
+        </v-btn>
+      </div>
+      <div class="d-flex flex secondary-controls">
+        <v-btn
+          icon
+          color="primary"
+          x-large
+          class="ma-3 animate__animated animate__fadeIn animate__slower"
+          :width="config.button.size"
+          :height="config.button.size"
+        >
+          <v-icon>mdi-music</v-icon>
+        </v-btn>
+
+        <v-btn
+          icon
+          color="primary"
+          x-large
+          class="ma-3 animate__animated animate__fadeIn animate__slower"
+          :width="config.button.size"
+          :height="config.button.size"
+        >
+          <v-icon>mdi-information-outline</v-icon>
+        </v-btn>
+
+        <v-btn
+          icon
+          color="primary"
+          x-large
+          class="ma-3 animate__animated animate__fadeIn animate__slower"
+          :width="config.button.size"
+          :height="config.button.size"
+        >
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
